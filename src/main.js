b14ea6c8de20ea6b41c062a38741c1101b09644b
otakustay <otakustay@gmail.com>
2013-10-19 22:41:02 +0800

    添加入口

---
+++ src/main.js
@@ -0,0 +1,15 @@
+/**
+ * mini-event
+ * Copyright 2013 Baidu Inc. All rights reserved.
+ * 
+ * @file 入口
+ * @author otakustay
+ */
+
+define(
+    function (require) {
+        return {
+            Event: require('./Event')
+        };
+    }
+);
